d634d1c2d4b606787cc65c0a0fb2f35ebac02cad
Romain de Reydellet <romain.dereydellet@gmail.com>
2016-10-20 18:49:07 +0200

    Update project to use auth

--- training.py
+++ training.py
@@ -7,6 +7,6 @@ training = Blueprint('training', __name__)
 @training.route('/training')
 def index():
 	if not user_auth():
-		return render_template('ajax.html', info=jsonify({'error':'n', 'msg':'You are not authenticated'}))
+		return redirect(url_for('index'))
 	bounties_info, information_count, xsslab_info, xsslab_count, targets_info, targets_count = extract_db()
 	return render_template('index.html', bounties=bounties_info, nbounties=information_count[0][0], ndollars=sum_reward(bounties_info), xsslab=xsslab_info, nxss=xsslab_count[0][0], targets=targets_info, ntargets=targets_count[0][0], page="training.html" )
